fef50497c11c4949c7c07943bc86fd5a8a9f51f5
Muhammad Attiq <m.attiq.ce@gmail.com>
2024-12-25 01:03:49 -0800

    Create Motion_Filtering.py

---
+++ Motion_Filtering.py
@@ -0,0 +1,23 @@
+import cv2 as cv
+
+video = cv.VideoCapture("people.mp4")
+
+subtractor = cv.createBackgroundSubtractorMOG2(500, 200)
+
+while True:
+
+    ret, frame = video.read()
+
+    if ret:
+        mask = subtractor.apply(frame)
+        cv.imshow("Mask", mask)
+
+        if cv.waitKey(5) == ord('X'):
+            break
+
+    else:
+        video = cv.VideoCapture("people.mp4")
+
+cv.destroyAllWindows()
+
+video.release()
